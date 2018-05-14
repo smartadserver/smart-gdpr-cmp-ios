18fdb2e3437de985255a8a5d9dc183b332cad002
Loïc GIRON DIT METAZ <loic.gironditmetaz@gmail.com>
2018-05-14 17:09:06 +0200

    Vendors list can be translated using an additional JSON (optional)

--- framework/SmartCMPTests/VendorList/CMPVendorListTests.swift
+++ framework/SmartCMPTests/VendorList/CMPVendorListTests.swift
@@ -21,15 +21,18 @@ class CMPVendorListTests : XCTestCase {
         return  formatter.date(from: string)!
     }
     
-    // Loading a sample JSON for unit testing
     private lazy var vendorsJSON: Data = {
         let url = Bundle(for: type(of: self)).url(forResource: "vendors", withExtension: "json")!
         return try! Data(contentsOf: url)
     }()
     
-    // Loading a sample JSON for unit testing
     private lazy var updatedVendorsJSON: Data = {
         let url = Bundle(for: type(of: self)).url(forResource: "vendors_updated", withExtension: "json")!
+        return try! Data(contentsOf: url)
+    }()
+    
+    private lazy var vendorsFRJSON: Data = {
+        let url = Bundle(for: type(of: self)).url(forResource: "vendors-fr", withExtension: "json")!
         return try! Data(contentsOf: url)
     }()
     
@@ -114,6 +117,63 @@ class CMPVendorListTests : XCTestCase {
         XCTAssertNotNil(decodedVendorList)
         
         XCTAssertEqual(vendorList, decodedVendorList)
+    }
+    
+    func testVendorListCannotBeCreatedFromTranslatedListOnly() {
+        let vendorList = CMPVendorList(jsonData: self.vendorsFRJSON)
+        XCTAssertNil(vendorList)
+    }
+    
+    func testVendorListCanBeLocalizedWithAnExternalJson() {
+        let vendorList = CMPVendorList(jsonData: self.vendorsJSON, localizedJsonData: self.vendorsFRJSON)
+        
+        XCTAssertNotNil(vendorList)
+        XCTAssertEqual(vendorList?.vendorListVersion, 6)
+        XCTAssertEqual(vendorList?.lastUpdated, date(from: "2018-04-23T16:03:22Z"))
+        
+        XCTAssertEqual(vendorList?.purposes.count, 5)
+        XCTAssertEqual(vendorList?.purposes[2].id, 3)
+        XCTAssertEqual(vendorList?.purposes[2].name, "Purpose 3 name translated in french")
+        XCTAssertEqual(vendorList?.purposes[2].purposeDescription, "Purpose 3 description translated in french")
+        
+        XCTAssertEqual(vendorList?.features.count, 3)
+        XCTAssertEqual(vendorList?.features[1].id, 2)
+        XCTAssertEqual(vendorList?.features[1].name, "Feature 2 name translated in french")
+        XCTAssertEqual(vendorList?.features[1].featureDescription, "Feature 2 description translated in french")
+        
+        XCTAssertEqual(vendorList?.vendors.count, 17)
+        XCTAssertEqual(vendorList?.vendors[4].id, 27)
+        XCTAssertEqual(vendorList?.vendors[4].name, "ADventori SAS")
+        XCTAssertEqual(vendorList?.vendors[4].policyURL, URL(string: "https://www.adventori.com/with-us/legal-notice/"))
+        XCTAssertEqual(vendorList?.vendors[4].purposes, [2])
+        XCTAssertEqual(vendorList?.vendors[4].legitimatePurposes, [1, 3, 4, 5])
+        XCTAssertEqual(vendorList?.vendors[4].features, [])
+    }
+    
+    func testVendorListWithInvalidLocalizedJsonFallbackOnDefaultValues() {
+        let vendorList = CMPVendorList(jsonData: self.vendorsJSON, localizedJsonData: "{}".data(using: String.Encoding.utf8)!) // Empty localized JSON
+        
+        XCTAssertNotNil(vendorList)
+        XCTAssertEqual(vendorList?.vendorListVersion, 6)
+        XCTAssertEqual(vendorList?.lastUpdated, date(from: "2018-04-23T16:03:22Z"))
+        
+        XCTAssertEqual(vendorList?.purposes.count, 5)
+        XCTAssertEqual(vendorList?.purposes[2].id, 3)
+        XCTAssertEqual(vendorList?.purposes[2].name, "Ad selection, delivery, reporting")
+        XCTAssertEqual(vendorList?.purposes[2].purposeDescription, "The collection of information, and combination with previously collected information, to select and deliver advertisements for you, and to measure the delivery and effectiveness of such advertisements. This includes using previously collected information about your interests to select ads, processing data about what advertisements were shown, how often they were shown, when and where they were shown, and whether you took any action related to the advertisement, including for example clicking an ad or making a purchase. ")
+        
+        XCTAssertEqual(vendorList?.features.count, 3)
+        XCTAssertEqual(vendorList?.features[1].id, 2)
+        XCTAssertEqual(vendorList?.features[1].name, "Linking Devices")
+        XCTAssertEqual(vendorList?.features[1].featureDescription, "Allow processing of a user's data to connect such user across multiple devices.")
+        
+        XCTAssertEqual(vendorList?.vendors.count, 17)
+        XCTAssertEqual(vendorList?.vendors[4].id, 27)
+        XCTAssertEqual(vendorList?.vendors[4].name, "ADventori SAS")
+        XCTAssertEqual(vendorList?.vendors[4].policyURL, URL(string: "https://www.adventori.com/with-us/legal-notice/"))
+        XCTAssertEqual(vendorList?.vendors[4].purposes, [2])
+        XCTAssertEqual(vendorList?.vendors[4].legitimatePurposes, [1, 3, 4, 5])
+        XCTAssertEqual(vendorList?.vendors[4].features, [])
     }
     
 }
